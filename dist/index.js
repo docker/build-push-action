cf7e6e140f8740b68c2adbf5b1171c647921d457
Jyotsna <Josh-01@github.com>
2020-11-11 20:19:52 +0530

    Added new trace-Data input. changed label value to link to dockerfile
    Signed-off-by: Jyotsna <Josh-01@github.com>

--- dist/index.js
+++ dist/index.js
@@ -12083,20 +12083,16 @@ function getInputs(defaultContext) {
             cacheTo: yield getInputList('cache-to', true),
             secrets: yield getInputList('secrets', true),
             githubToken: core.getInput('github-token'),
-            ssh: yield getInputList('ssh')
+            ssh: yield getInputList('ssh'),
+            traceData: core.getInput('trace-data') || 'false'
         };
-        if (userInputs.load == true ||
-            userInputs.push == true ||
-            userInputs.outputs.find(val => val.indexOf('type=image') > -1 || val.indexOf('type=registry') > -1)) {
-            //Add repo as source-label if not already supplied by user
-            const sourceLabelKey = 'org.opencontainers.image.source';
-            if (userInputs.labels.find(val => val.startsWith(sourceLabelKey) == true) == null) {
-                const githubOwnerRepoUrl = defaultContext.split('#')[0];
-                userInputs.labels.push(`${sourceLabelKey}=${githubOwnerRepoUrl}`);
-            }
-            //Add dockerfile path as label
+        if (userInputs.traceData == 'true' && //if user explictly asks to add traceData
+            (userInputs.load == true ||
+                userInputs.push == true ||
+                userInputs.outputs.find(val => val.indexOf('type=image') > -1 || val.indexOf('type=registry') > -1))) {
+            //Add link to dockerfile as label
             let dockerfilePath = userInputs.file;
-            userInputs.labels.push(`dockerfile-path=${dockerfilePath}`);
+            userInputs.labels.push(`dockerfile-path=${defaultContext}/${dockerfilePath}`);
         }
         return userInputs;
     });
