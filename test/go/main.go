a3646c08f829bf95ff76c8cb6e6ef2331dcb2506
CrazyMax <crazy-max@users.noreply.github.com>
2023-04-09 06:33:30 +0200

    test: update go example
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- test/go/main.go
+++ test/go/main.go
@@ -1,31 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-	"os"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, Go!")
 	})
-
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("HTTP_PORT")
-	if httpPort == "" {
-		httpPort = "8080"
-	}
-
-	e.Logger.Fatal(e.Start(":" + httpPort))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
